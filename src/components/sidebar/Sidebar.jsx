2fa5b505f14b8da4ebaede34f9aa6edd6a47d020
dutt.mistry <dutt.mistry@cybercom.in>
2025-01-28 10:37:24 +0530

    Sidebar Design Changes

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,29 +1,34 @@
-import React from 'react'
-import './sidebar.css'
-const Sidebar = ({doctorsData}) => {
+import React from "react";
+import "./sidebar.css";
+const Sidebar = ({ doctorsData }) => {
   return (
-<div className="table-container">
-<table className="indian-states-table">
-  <thead>
-    <tr>
-      <th colSpan={3} className="main-header">DATA</th>
-    </tr>
-    <tr>
-      <th>PinCode</th>
-      <th>Visits</th>
-    </tr>
-  </thead>
-  <tbody>
-    {doctorsData.map((item, index) => (
-      <tr key={item.pin_code} className={index % 2 === 0 ? 'even-row' : 'odd-row'}>
-        <td>{item.pin_code}</td>
-        <td className="info-cell">{item.count}</td>
-      </tr>
-    ))}
-  </tbody>
-</table>
-</div>
-  )
-}
+    <div className="table-container">
+      <table className="indian-states-table">
+        <thead>
+          <tr>
+            <th colSpan={2} className="main-header">
+              DATA
+            </th>
+          </tr>
+          <tr>
+            <th>PinCode</th>
+            <th>Visits</th>
+          </tr>
+        </thead>
+        <tbody>
+          {doctorsData.map((item, index) => (
+            <tr
+              key={item.pin_code}
+              className={index % 2 === 0 ? "even-row" : "odd-row"}
+            >
+              <td>{item.pin_code}</td>
+              <td className="info-cell">{item.count}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
